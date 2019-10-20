764ea921ba94ad3aeb22ea5fb1c4777ac6a4e497
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-10-20 18:14:45 +0200

    add GloVeEmbedding Class for Word Embeddings in tf

--- src/model.py
+++ src/model.py
@@ -7,15 +7,15 @@ from tensorflow.keras.layers import Dense, Embedding, Conv1D, CuDNNLSTM, Input
 from tensorflow.keras.models import Model
 
 from src.etl import get_data_and_label
-
+from src.modelhelper import GloVeEmbeddings
+from src.modelhelper import Dimension
 
 class DummyModel:
 
-    def __init__(self, number_words: int = 20000, sequence_length: int = 50, embedding_size: int = 50,
-                 embeddings=None):
+    def __init__(self, number_words: int = 20000, dimension: Dimension = Dimension.d100):
         self.number_words = number_words
-        self.sequence_length = sequence_length
-        self.embedding_size = embedding_size
+        self.sequence_length = dimension.value
+        self.embedding_size = dimension.value
 
     def _create_network(self):
         pass
@@ -26,6 +26,10 @@ class DummyModel:
     def _tokenize_data(self, data):
         tokenizer = Tokenizer(num_words=20000)
         tokenizer.fit_on_texts(data)
+
+        embeddings = GloVeEmbeddings()
+        embeddings.prepare_embedding_file(tokenizer.word_index, self.number_words)
+
         data = tokenizer.texts_to_sequences(data)
         data = pad_sequences(data, maxlen=self.sequence_length)
         return data
@@ -35,11 +39,11 @@ class DummyModel:
 
     def _build_network(self, batch_size: int = 1024, epochs: int = 10):
         input = Input(shape=(self.sequence_length, ))
-        x = Embedding(self.number_words, embedding_size, weights=[matr])(input)
+        x = Embedding(self.number_words, self.embedding_size, weights=[matrix])(input)
         x = Conv1D()(x)
         x = CuDNNLSTM()(x)
         x = Dense()(x)
-        self.model = Model(inputs=inp, output=x)
+        self.model = Model(inputs=input, output=x)
         self.model.compile(loss="binary_crossentropy", optimizer="adam")
 
 
--- src/modelhelper.py
+++ src/modelhelper.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import numpy as np
 
 
 class Dimension(Enum):
@@ -17,7 +18,28 @@ class GloVeEmbeddings:
     def __init__(self, path: str = "../data/", dimension: Dimension = Dimension.d100):
         self.path = path
         self.dimension = dimension
-        self.file_path = "{}glove.twitter.27B.{}d.zip".format(path, dimension.value)
+        self.file_path = "{}glove.twitter.27B.{}d.txt".format(path, dimension.value)
+        self.embedding_mean = None
+        self.embedding_std = None
+        self.embedding_size = None
+        self.number_words = None
 
-    def prepare_embedding_file(self):
-        pass
+    def prepare_embedding_file(self, word_index, number_words: int = 20000):
+        def convert_to_array(row):
+            return row[0], np.array(row[1:], dtype="float32")
+
+        with open(self.file_path, "r", encoding="utf-8") as f:
+            embeddings = dict(convert_to_array(row.rstrip().rsplit(" ")) for row in f.readlines())
+
+        embeddings_matrix = np.stack(embeddings.values())
+        self.embedding_mean, self.embedding_std = embeddings_matrix.mean(), embeddings_matrix.std()
+        self.embedding_size = embeddings_matrix.shape[1]
+        self.number_words = min(len(word_index), number_words)
+        print(embeddings)
+
+
+
+
+if __name__ == "__main__":
+    embedding = GloVeEmbeddings()
+    embedding.prepare_embedding_file()
